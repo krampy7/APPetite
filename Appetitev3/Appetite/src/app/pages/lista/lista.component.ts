4f476181fb5fecbed32acccfa7a13b11dcd5ebb0
Krampy <adrian.cano.r9@gmail.com>
2021-07-11 13:05:27 -0500

    Lista directory was updated

--- Appetitev3/Appetite/src/app/pages/lista/lista.component.ts
+++ Appetitev3/Appetite/src/app/pages/lista/lista.component.ts
@@ -8,20 +8,27 @@ import { FirestoreService } from 'src/app/services/firestore.service';
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.scss'],
 })
+
+//Class ListaComponent
 export class ListaComponent implements OnInit {
 
+  //Array called locales that saves all the locals
   locales: Local[] = [];
 
   private path = 'Locales/';
 
+  //Constructor with services loaded
   constructor(public modalController: ModalController,
     public database:FirestoreService,) { }
 
+  //When the page starts, method getLocales will be called
   ngOnInit() {
     this.getLocales();
   }
 
+  //Method getLocales
   getLocales() {
+    //Gets all the locales from the database and store them in the array locales
     this.database.getCollection<Local>(this.path).subscribe(res => {
       this.locales = res;
     });
