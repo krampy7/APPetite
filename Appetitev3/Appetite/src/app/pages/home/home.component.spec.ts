7dbc6447f473d39052a9c247eba6633b6c55bc20
Krampy <adrian.cano.r9@gmail.com>
2021-07-11 12:55:23 -0500

    Directory home was updated

--- Appetitev3/Appetite/src/app/pages/home/home.component.spec.ts
+++ Appetitev3/Appetite/src/app/pages/home/home.component.spec.ts
@@ -22,3 +22,4 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+s
--- Appetitev3/Appetite/src/app/pages/home/home.component.ts
+++ Appetitev3/Appetite/src/app/pages/home/home.component.ts
@@ -1,55 +1,78 @@
-import { User } from './../../models';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
+import { User } from './../../models';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+
+//Class HomeComponent
 export class HomeComponent implements OnInit {
 
+  //Interface user
    user:User;
 
+  //Constructor with services
   constructor(public authSvc:FirebaseauthService,
     private router: Router) {}
 
+  //ngOnit()
   async ngOnInit() {
+    //Subscription
     this.authSvc.stateAuth().subscribe(res=>{
+      //If there is no user
       if(res === null){
         console.log('No has inicido sesión')
 
-      }else{
+      }
+      //If there is a user
+      else{
+        //If user's id is equals to dWoZ8iNFmeUfAL3eNWUEVPH9IrC3
         if (res.uid === 'dWoZ8iNFmeUfAL3eNWUEVPH9IrC3') {
+          //Redirect to admin page
           this.router.navigate(['admin']);
         }
+        //If it's not dWoZ8iNFmeUfAL3eNWUEVPH9IrC3
         else{
+          //Redirect to page lista
           this.router.navigate(['tabs/lista']);
         }
       }
     });
   }
 
+  //onLoginGoogle method
   async onLoginGoogle(){
     try{
+      //Method login() is being called
       this.user = await this.authSvc.login();
+      //If there is a user
       if (this.user) {
+        //Display in console the id of the user
         console.log('user->', this.user.uid);
-        //this.redirectUser(this.user.uid);
+        //Calls the method redirectUser from the actual object
         this.redirectUser(this.user.uid);
       }
-    }catch (error){
+    }
+    //If there is an error
+    catch (error){
+      //Display error in console
       console.log('Error->', error);
     }
   }
 
+  //Method with an user's id as a parameter
   private redirectUser(user_id:string){
-    //redirect -> admin
+    //If the user's id is dWoZ8iNFmeUfAL3eNWUEVPH9IrC3
     if (user_id == 'dWoZ8iNFmeUfAL3eNWUEVPH9IrC3') {
+      //Redirect to page admin
       this.router.navigate(['admin']);
     }
     else{
+      //Redirect to page lista
       this.router.navigate(['tabs/lista']);
     }
   }
